8a18e3cafd74c28b221d08bbbf009e637d55c595
aborel <austincborel@gmail.com>
2026-06-17 18:37:10 +0200

    changed password management

---
+++ Makefile
@@ -0,0 +1,6 @@
+
+env:
+	@cp .env.template .env
+	@echo "\033[0;31mREMEMBER TO SET UP PASSWORDS\033[0m\n"
+
+
--- srcs/requirements/wordpress/conf/auto_config.sh
+++ srcs/requirements/wordpress/conf/auto_config.sh
@@ -1,9 +1,31 @@
 sleep 10
-wp config create --allow-root \
-	--db_name=$SQL_DATABASE \
-	--dbuser=$SQL_USER \
-	--dbpass=$SQL_PASSWORD \
-	--dbhost=mariadb:3306 --path='/var/www/wordpress'
 
-wp user create $SQL_USER $SQL_EMAIL --role=administrator
+#if WordPress isn't already set up then
+if [ ! -f /var/www/wordpress/wp-config.php ]; then
+	#writes wp-config.php
+	wp config create --allow-root \
+		--db_name=$SQL_DATABASE \
+		--dbuser=$SQL_USER \
+		--dbpass=$SQL_PASSWORD \
+		--dbhost=mariadb:3306 \
+		--path=/var/www/wordpress
+	
+	#does the WordPress installation and sets up admin
+	wp core install --allow-root \
+		--url=$DOMAIN_NAME \
+		--title="My Site" \
+		--admin_user=$WP_ADMIN \
+		--admin_password=$WP_ADMIN_PASS \
+		--admin-email=$WP_ADMIN_EMAIL \
+		--path=/var/www/wordpress
+
+	#creates additional user
+	wp user create --allow-root \
+		$WP_USER $WP_USER_EMAIL \
+		--role=subscriber \
+		user_pass=$WP_USER_PASS \
+		--path=/var/www/wordpress
+fi
+
+exec php-fpm8.2 -F
 
