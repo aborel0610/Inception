b7f55510474973d012a20972e81ff5d8df51a694
aborel <aborel@student.42.fr>
2026-06-17 13:44:32 +0200

    wp-config.php

--- srcs/requirements/wordpress/conf/wp-config.php
+++ srcs/requirements/wordpress/conf/wp-config.php
@@ -20,16 +20,16 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'database_name_here' );
+define( 'DB_NAME', 'Inception' );
 
 /** Database username */
-define( 'DB_USER', 'username_here' );
+define( 'DB_USER', 'user' );
 
 /** Database password */
-define( 'DB_PASSWORD', 'password_here' );
+define( 'DB_PASSWORD', '1234' );
 
 /** Database hostname */
-define( 'DB_HOST', 'localhost' );
+define( 'DB_HOST', 'mariadb' );
 
 /** Database charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8mb4' );
@@ -48,14 +48,14 @@ define( 'DB_COLLATE', '' );
  *
  * @since 2.6.0
  */
-define( 'AUTH_KEY',         'put your unique phrase here' );
-define( 'SECURE_AUTH_KEY',  'put your unique phrase here' );
-define( 'LOGGED_IN_KEY',    'put your unique phrase here' );
-define( 'NONCE_KEY',        'put your unique phrase here' );
-define( 'AUTH_SALT',        'put your unique phrase here' );
-define( 'SECURE_AUTH_SALT', 'put your unique phrase here' );
-define( 'LOGGED_IN_SALT',   'put your unique phrase here' );
-define( 'NONCE_SALT',       'put your unique phrase here' );
+define('AUTH_KEY',         'dq~c/iE6++!KkXgPIa|w@5jmrd cfU{wkz(0h>4O~>&J>E}?2sb@,:OFFk>P,3%(');
+define('SECURE_AUTH_KEY',  'r*s{9;qmAaDR7)qGeX-a-V/y{|I;y$majReXyycG){1OYHa}Y@SW`8- [ZiKLUM`');
+define('LOGGED_IN_KEY',    'bI`hR<C&^<M }+c_eLi-|odoO4e[>P^qfMGrVJA44;D-b!f5%WC$-Bz2YJ0Bf<Lz');
+define('NONCE_KEY',        'nT#&vFQA7!BH-5!;/SS4nie0Crnn^2+)|D8x]/93DPd#S.WjaslVDa}^@_A6wmBq');
+define('AUTH_SALT',        'KutJA3-m6h~Z$VX {c/IJz9*X+|mp;$h|{9r 61..,-44b$g4_%}be+W=^` `Tm:');
+define('SECURE_AUTH_SALT', '3DWa_CNdE +!$2<USm;J,.SHgahz`Kv8JbC)brpk-JE@$e2FPW{Xk`=/W?`}2e.G');
+define('LOGGED_IN_SALT',   'G8y!V;{|4V}+,-nX:hMhC,`Y%E)4UWG!S/+8@nM1y$T}3LJjL38O=1>1.r<k3Cah');
+define('NONCE_SALT',       'bU~G:E7.IW,)L!XVpR+o{2|0,Vw?H,d77m+z<QrNTj11r#.%jqHS|$r~foK<*w5T');
 
 /**#@-*/
 
@@ -85,11 +85,9 @@ $table_prefix = 'wp_';
  *
  * @link https://developer.wordpress.org/advanced-administration/debug/debug-wordpress/
  */
-define( 'WP_DEBUG', false );
+define( 'WP_DEBUG', true );
 
 /* Add any custom values between this line and the "stop editing" line. */
-
-
 
 /* That's all, stop editing! Happy publishing. */
 
