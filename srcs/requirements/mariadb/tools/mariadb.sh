66c4e183f2e638fb8b364ee079dda546ea8aeebc
aborel <austincborel@gmail.com>
2026-06-18 16:25:57 +0200

    mariadb dockerfile fix

--- srcs/requirements/mariadb/tools/mariadb.sh
+++ srcs/requirements/mariadb/tools/mariadb.sh
@@ -1,9 +1,11 @@
-service mysql start
+#!/bin/bash
+
+mysqld_safe &
 
 mysql -e "CREATE DATABASE IF NOT EXISTS \'${SQL_DATABASE}\';"
 mysql -e "CREATE USER IF NOT EXISTS \'${SQL_USER}\'@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
 mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
 mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
 mysql -e "FLUSH PRIVILEGES;"
-mysqladmin -uroot -p$SQL_PASSWORD $SQL_DATABASE < /usr/local/bin/inception.sql
-exec mysqld_safe
+
+exec mysqld
